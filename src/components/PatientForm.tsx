72b635204fb7b573e34066f9daca881134085371
BryanGallo <bryangallo95@hotmail.com>
2026-01-04 13:45:35 -0500

    feat: validaciones campos y type para useForm

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -1,14 +1,16 @@
-import { useForm } from "react-hook-form";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import Error from "../ui/Error";
+import type { Patient } from "../types";
 
 export default function PatientForm() {
     const {
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm();
+    } = useForm<Patient>();
 
-    const registerPatient = () => {
+    const registerPatient: SubmitHandler<Patient> = (data) => {
+        console.log(data);
     };
 
     return (
@@ -53,7 +55,11 @@ export default function PatientForm() {
                             {errors.name.message as string}
                         </p>
                     ) : null} */}
-                    {errors.name ? <Error message={errors.name.message?.toString() || ""} /> : null}
+                    {errors.name ? (
+                        <Error
+                            message={errors.name.message?.toString() || ""}
+                        />
+                    ) : null}
                 </div>
 
                 <div className="mb-5">
@@ -68,7 +74,21 @@ export default function PatientForm() {
                         className="w-full p-3  border border-gray-100"
                         type="text"
                         placeholder="Nombre del Propietario"
+                        {...register("caretaker", {
+                            required:
+                                "El nombre del propietario es obligatorio",
+                                minLength: {
+                                    value: 4,
+                                    message:
+                                        "El nombre del propietario debe tener al menos 4 caracteres",
+                                },
+                        })}
                     />
+                    {errors.caretaker ? (
+                        <Error
+                            message={errors.caretaker.message?.toString() || ""}
+                        />
+                    ) : null}
                 </div>
 
                 <div className="mb-5">
@@ -83,7 +103,19 @@ export default function PatientForm() {
                         className="w-full p-3  border border-gray-100"
                         type="email"
                         placeholder="Email de Registro"
+                        {...register("email", {
+                            required: "El email es obligatorio",
+                            pattern: {
+                                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                message: "El email no es válido",
+                            },
+                        })}
                     />
+                    {errors.email ? (
+                        <Error
+                            message={errors.email.message?.toString() || ""}
+                        />
+                    ) : null}
                 </div>
 
                 <div className="mb-5">
@@ -97,7 +129,19 @@ export default function PatientForm() {
                         id="date"
                         className="w-full p-3  border border-gray-100"
                         type="date"
+                        {...register("date", {
+                            required: "La fecha es obligatoria",
+                            min: { 
+                                value: new Date().toISOString().split("T")[0],
+                                message: "La fecha debe ser igual o mayor a la fecha actual",
+                            },
+                        })}
                     />
+                    {errors.date ? (
+                        <Error
+                            message={errors.date.message?.toString() || ""}
+                        />
+                    ) : null}
                 </div>
 
                 <div className="mb-5">
@@ -111,7 +155,15 @@ export default function PatientForm() {
                         id="symptoms"
                         className="w-full p-3  border border-gray-100"
                         placeholder="Síntomas del paciente"
+                        {...register("symptoms", {
+                            required: "Los síntomas son obligatorios",
+                        })}
                     ></textarea>
+                    {errors.symptoms ? (
+                        <Error
+                            message={errors.symptoms.message?.toString() || ""}
+                        />
+                    ) : null}
                 </div>
 
                 <input
