54dc97817b2a5dde60cac79e7c1ca878a4ed81e3
BryanGallo <bryangallo95@hotmail.com>
2026-01-04 09:33:56 -0500

    feat: estructura inicial de formulario y lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import PatientForm from "./components/PatientForm";
+import PatientList from "./components/PatientList";
 
 function App() {
     return (
-        <div>
-            <h1>Hola mundo</h1>
-        </div>
+        <>
+            <div className="container mx-auto bg-red-100 p-4 rounded-lg shadow-md">
+                <h1 className="text-2xl font-bold text-center md:w-2/3 mx-auto">
+                    Seguimiento de pacientes{" "}
+                    <span className="text-blue-500">Nexus</span>
+                </h1>
+            </div>
+            <div className="m-12 md:flex">
+                <div className="md:w-2/5">
+                    <PatientForm />
+                </div>
+                <div className="md:w-3/5">
+                    <PatientList />
+                </div>
+            </div>
+        </>
     );
 }
 
