b6152627fc756c1ccd9d1eccfc8881f0f5e86c47
BryanGallo <bryangallo95@hotmail.com>
2026-01-04 13:31:18 -0500

    refactor: estilo de mensaje y tamaño de columnas

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import PatientList from "./components/PatientList";
 function App() {
     return (
         <>
-            <div className="container mx-auto bg-red-100 p-4 rounded-lg shadow-md">
+            <div className="container mx-auto p-4 rounded-lg shadow-md">
                 <h1 className="text-2xl font-bold text-center md:w-2/3 mx-auto">
                     Seguimiento de pacientes{" "}
                     <span className="text-blue-500">Nexus</span>
                 </h1>
             </div>
             <div className="m-12 md:flex">
-                <div className="md:w-2/5">
+                <div className="md:w-1/2">
                     <PatientForm />
                 </div>
-                <div className="md:w-3/5">
+                <div className="md:w-1/2">
                     <PatientList />
                 </div>
             </div>
--- src/ui/Error.tsx
+++ src/ui/Error.tsx
@@ -1,3 +1,3 @@
 export default function Error({ message }: { message: string }) {
-    return <p className="text-white bg-red-500 p-2 rounded-md text-sm mt-2">{message}</p>;
+    return <p className="text-white bg-red-500 p-2 text-sm mt-2">{message}</p>;
 }
