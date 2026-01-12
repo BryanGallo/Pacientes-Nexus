551d853a7ba4d473d30db3ce318754bd49fb5534
BryanGallo <bryangallo95@hotmail.com>
2026-01-12 08:22:21 -0500

    add: boton editar - eliminar

--- src/components/PatientCard.tsx
+++ src/components/PatientCard.tsx
@@ -1,8 +1,9 @@
 import type { Patient } from "../types";
+import { usePatientStore } from "../store/store";
 
 export default function PatientCard({ patient }: { patient: Patient }) {
+    const { deletePatient } = usePatientStore();
     return (
-        console.log(patient),
         (
             <div className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
                 <h3 className="text-2xl font-bold">{patient.name}</h3>
@@ -17,6 +18,8 @@ export default function PatientCard({ patient }: { patient: Patient }) {
                     Síntomas: {patient.symptoms}
                 </p>
                 <p className="text-sm text-gray-500">Email: {patient.email}</p>
+                <button type="button" className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer" onClick={() => deletePatient(patient.id)}>Editar Paciente</button>
+                <button type="button" className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer" onClick={() => deletePatient(patient.id)}>Eliminar Paciente</button>
             </div>
         )
     );
