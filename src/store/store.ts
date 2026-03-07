f9cfd7d92e9cd14958a8fdff826a7f2494349bd9
BryanGallo <bryangallo95@hotmail.com>
2026-03-07 08:54:36 -0500

    feat: devtools para ver el estado en el navegador
    Instalar Redux Devtools

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 import type { DraftPatient, Patient } from "../types";
 
 type PatientState = {
@@ -11,43 +12,45 @@ type PatientState = {
     clearActivePatient: () => void;
 };
 
-export const usePatientStore = create<PatientState>((set) => ({
-    patients: [],
-    activePatient: "",
-    addPatient: (data: DraftPatient) => {
-        const newPatient: Patient = {
-            ...data,
-            //* Genera un ID aleatorio usando el api
-            //* https://developer.mozilla.org/es/docs/Web/API/Crypto/randomUUID
-            id: crypto.randomUUID(),
-        };
-        set((state) => ({
-            patients: [...state.patients, newPatient],
-        }));
-    },
-    deletePatient: (id) => {
-        set((state) => ({
-            patients: state.patients.filter((patient) => patient.id !== id),
-        }));
-    },
-    getActivePatient: (id) => {
-        set(() => ({
-            activePatient: id,
-        }));
-    },
-    updatePatient: (data) => {
-        set((state) => ({
-            patients: state.patients.map((patient) =>
-                patient.id === state.activePatient
-                    ? { id: state.activePatient, ...data }
-                    : patient,
-            ),
-            activePatient: "",
-        }));
-    },
-    clearActivePatient: () => {
-        set(() => ({
-            activePatient: "",
-        }));
-    },
-}));
+export const usePatientStore = create<PatientState>()(
+    devtools((set) => ({
+        patients: [],
+        activePatient: "",
+        addPatient: (data: DraftPatient) => {
+            const newPatient: Patient = {
+                ...data,
+                //* Genera un ID aleatorio usando el api
+                //* https://developer.mozilla.org/es/docs/Web/API/Crypto/randomUUID
+                id: crypto.randomUUID(),
+            };
+            set((state) => ({
+                patients: [...state.patients, newPatient],
+            }));
+        },
+        deletePatient: (id) => {
+            set((state) => ({
+                patients: state.patients.filter((patient) => patient.id !== id),
+            }));
+        },
+        getActivePatient: (id) => {
+            set(() => ({
+                activePatient: id,
+            }));
+        },
+        updatePatient: (data) => {
+            set((state) => ({
+                patients: state.patients.map((patient) =>
+                    patient.id === state.activePatient
+                        ? { id: state.activePatient, ...data }
+                        : patient,
+                ),
+                activePatient: "",
+            }));
+        },
+        clearActivePatient: () => {
+            set(() => ({
+                activePatient: "",
+            }));
+        },
+    })),
+);
